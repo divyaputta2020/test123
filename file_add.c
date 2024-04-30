d643118f12d7faa4f872d4b1e43225e8b731b459
divyaputta2020 <divyaputta2020@gmail.com>
2024-04-30 14:36:37 +0000

    added new function

--- file_add.c
+++ file_add.c
@@ -1,1 +1,22 @@
-
+[200~#include <stdio.h>
+ 
+int main() {
+	 
+	int num1, num2, sum;
+	// Input the two numbers
+	// printf("Enter first number: ");
+	// scanf("%d", &num1);
+	// printf("Enter second number: ");
+	// scanf("%d", &num2);
+	// Add the numbers
+	// sum num1 + num2;
+	// Display the result
+	// printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+	// return 0;
+	// }
+	//   int addfucn(int a, int b)
+	//    {
+	//      int c=a+b;
+	//      return c;
+	//    }
+	// added in branch1
