a65bd3098d2ffc4c640232189d4e8701eeca221b
divyaputta2020 <divyaputta2020@gmail.com>
2024-04-30 14:56:30 +0000

    added same function to main branch

--- file_add.c
+++ file_add.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+ 
+int main() {
+ 
+int num1, num2, sum;
+ 
+// Input the two numbers
+ 
+printf("Enter first number: ");
+ 
+scanf("%d", &num1);
+ 
+printf("Enter second number: ");
+ 
+scanf("%d", &num2);
+ 
+// Add the numbers
+ 
+sum num1 + num2;
+ 
+// Display the result
+ 
+printf("The sum of %d and %d is: %d\n", num1, num2, sum);
